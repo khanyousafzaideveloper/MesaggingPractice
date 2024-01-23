2c784dde0fa4005b3a06e4cf079d50a5fa6d7638
Ahmad <khanyousafzaideveloper@gmail.com>
2024-01-23 19:23:22 +0500

    Simple messaging UI

--- app/src/main/java/com/example/messagespractice/MessagingScreen.kt
+++ app/src/main/java/com/example/messagespractice/MessagingScreen.kt
@@ -1,13 +1,16 @@
 package com.example.messagespractice
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,53 +20,79 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
+@SuppressLint("SuspiciousIndentation")
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun messagingScreen(){
-    var message by remember { mutableStateOf("") }
-    var messagesList by remember { mutableStateOf(listOf(message)) }
-    Column(modifier= Modifier.fillMaxSize()) {
-        Card(modifier= Modifier
-            .padding(16.dp)
-            .background(Color.Blue)) {
-            chatbox(message = message)
+fun MessagingScreen() {
+    Column (modifier= Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top){
+
+
+        var message by remember { mutableStateOf("") }
+        var texts by remember {
+            mutableStateOf(listOf<String>())
         }
-//        LazyColumn(
-//            modifier = Modifier
-//                .weight(1f)
-//                .fillMaxWidth()
-//        ) {
-//            items(message) { message ->
-//                chatbox(message = message)
-//            }
+        //val messagesList: MutableList<String> = mutableListOf("Hello ", "Hi" , "What's up")
+
+//    Column(modifier= Modifier.fillMaxSize()) {
+//        Card() {
+//            chatbox(message = message)
+//            chatbox(message = message)
+//            chatbox(message = message)
+//            chatbox(message = message)
+//            chatbox(message = message)
 //        }
-        Row(modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
-            
-            OutlinedTextField(value =message , onValueChange = {message=it}, Modifier.padding(end = 4.dp))
-            Button(onClick = { message }) {
-                Text(text = "Send")
+
+        LazyColumn(modifier = Modifier.weight(1f) , verticalArrangement = Arrangement.Bottom, horizontalAlignment = Alignment.End) {
+            items(texts) { messages ->
+                Card(
+                    modifier = Modifier
+                        .padding(16.dp)
+                ) {
+                    Text(text = messages)
+                }
             }
         }
-      }
+        fun OnSend() {
+            if (message.isNotBlank()) {
+                texts = texts + message
+                message = ""
+            }
 
-}
+        }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                OutlinedTextField(
+                    value = message,
+                    onValueChange = { message = it },
+                    Modifier.padding(end = 4.dp)
+                )
+                Button(onClick = { OnSend() }) {
+                    Text(text = "Send")
+                }
+            }
+        }
+    }
 @Composable
-fun chatbox(message: String){
+fun Chatbox(message: String){
     Text(text = message, fontSize = 32.sp, fontWeight = FontWeight(400))
 }
+
