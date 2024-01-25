fc6ae785175b0519f11e3fb4c684697e04a29434
Ahmad <khanyousafzaideveloper@gmail.com>
2024-01-25 17:01:24 +0500

    Coloring and shape of card

--- app/src/main/java/com/example/messagespractice/MessagingScreen.kt
+++ app/src/main/java/com/example/messagespractice/MessagingScreen.kt
@@ -1,17 +1,17 @@
 package com.example.messagespractice
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,60 +22,56 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
-@SuppressLint("SuspiciousIndentation")
+@SuppressLint("SuspiciousIndentation", "RememberReturnType")
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun MessagingScreen() {
     Column (modifier= Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top){
-
-
         var message by remember { mutableStateOf("") }
         var texts by remember {
             mutableStateOf(listOf<String>())
         }
 
-        LazyColumn(modifier = Modifier
+
+
+            LazyColumn(modifier = Modifier
             .weight(1f)
             .fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
             items(texts) { messages ->
-                Row(modifier = Modifier.fillMaxWidth() , horizontalArrangement = Arrangement.End) {
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
                     Card(
                         modifier = Modifier
-                            .padding(16.dp)
-                            .background(Color.Blue)
-                            .fillMaxWidth(0.45f)
+                            .padding(end = 16.dp, bottom = 16.dp, top = 16.dp, start = 40.dp)
+                            .border(3.dp, Color.Black, RoundedCornerShape(7.dp, 7.dp, 50.dp, 7.dp))
                     ) {
+
                         Text(
                             text = messages,
                             fontWeight = FontWeight(500),
                             modifier = Modifier
-                                .padding(8.dp)
+                                .padding(16.dp)
                                 .align(Alignment.Start)
                         )
                     }
                 }
                 Card(
                     modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .padding(16.dp)
-                        .background(Color.Magenta)
+                        .padding(16.dp, end = 40.dp)
+                        .border(3.dp, Color.Green, RoundedCornerShape(7.dp, 7.dp, 7.dp, 40.dp))
                 ) {
                     Text(text = messages, fontWeight = FontWeight(500), modifier = Modifier
-                        .padding(8.dp) .background(Color(66 ,133, 244, 43, )))
+                        .padding(16.dp)
+                    )
                 }
             }
         }
@@ -97,7 +93,10 @@ fun MessagingScreen() {
                 OutlinedTextField(
                     value = message,
                     onValueChange = { message = it },
-                    Modifier.padding(end = 4.dp)
+                    label = { Text(text = "Type a message")},
+                    maxLines = 2,
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+                    modifier = Modifier.padding(end = 4.dp) .weight(1f)
                 )
                 Button(onClick = { OnSend() }) {
                     Text(text = "Send")
